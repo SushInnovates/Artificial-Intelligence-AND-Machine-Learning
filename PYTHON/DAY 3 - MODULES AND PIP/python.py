9ab269c45d37efbf2f4e4d1c9091d6ec9dfcfbfa
Sushant Sutar <sushant.sutR@gmail.com>
2025-10-18 23:32:55 +0530

    Add files via upload

---
+++ PYTHON/DAY 3 - MODULES AND PIP/python.py
@@ -0,0 +1,7 @@
+import pandas
+import tensorflow #coz we dont have tensorflow installed yet
+import hashlib # it is built in module
+print("Hello")
+
+# external modules are need to be installed before using them, 
+# where built in are already installed so we dont need to install them seperetaly
