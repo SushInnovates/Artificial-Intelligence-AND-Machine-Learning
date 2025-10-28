509a078c0a210f1f64e3e8ff187f10eac12584cf
Sushant Sutar <sushant.sutR@gmail.com>
2025-10-28 20:51:58 +0530

    Add files via upload

---
+++ PYTHON/DAY 5 - COMMENTS, ESCAPE SEQUENCE & STATEMENTS/main.py
@@ -0,0 +1,5 @@
+# written by me
+# on a special day 
+# Written on  9 June 2025
+print("To the Miss Perfectionist :\t")
+print("\tTo others, they call her ‘she,’\n\tBut the word ‘she’ is the universe itself.\n\tYes, there are countless universes in space,\n\tBut to me, she is the only universe.\n\tUntil I realized, she is the verse,\n\tWoven into my tangled heart.")
