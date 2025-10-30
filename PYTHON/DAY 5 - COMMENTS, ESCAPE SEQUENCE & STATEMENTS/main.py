ab110f0c54ee283c590d099cb4099a832cd512f3
Sushant Sutar <sushant.sutR@gmail.com>
2025-10-30 20:34:25 +0530

    Update comments and print statements in main.py
    Added explanations for 'sep' and 'end' parameters in print function.

--- PYTHON/DAY 5 - COMMENTS, ESCAPE SEQUENCE & STATEMENTS/main.py
+++ PYTHON/DAY 5 - COMMENTS, ESCAPE SEQUENCE & STATEMENTS/main.py
@@ -7,9 +7,15 @@ used as a comment.
 It’s not executed unless assigned or printed.
 
 to write something in double qoutes ,we use :  /"   /"
+
+sep="seperator" is used to seperate multiple values 
+
+end="end" is used to specify what to print at the end default is \n
 """
 
 print("To the Miss Perfectionist :\t")
 
 print("\tTo others, they call her ‘she,’\n\tBut the word ‘she’ is the universe itself.\n\tYes, there are countless universes in space,\n\tBut to me, she is the only universe.\n\tUntil I realized, she is the verse,\n\tWoven into my tangled \"heart.\"")
 
+print("THANK YOU",2,11, sep ="~",end="0264\n")
+print("TSCHUSS")
