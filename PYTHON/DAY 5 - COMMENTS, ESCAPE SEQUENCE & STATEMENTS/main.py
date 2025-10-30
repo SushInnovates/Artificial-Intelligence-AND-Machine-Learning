dc2cbe5d5ccd4001712bb1b29dd79194cbc68860
Sushant Sutar <sushant.sutR@gmail.com>
2025-10-30 20:24:10 +0530

    Update main.py

--- PYTHON/DAY 5 - COMMENTS, ESCAPE SEQUENCE & STATEMENTS/main.py
+++ PYTHON/DAY 5 - COMMENTS, ESCAPE SEQUENCE & STATEMENTS/main.py
@@ -5,8 +5,11 @@
 This is a multi-line string
 used as a comment.
 It’s not executed unless assigned or printed.
+
+to write something in double qoutes ,we use :  /"   /"
 """
 
 print("To the Miss Perfectionist :\t")
 
-print("\tTo others, they call her ‘she,’\n\tBut the word ‘she’ is the universe itself.\n\tYes, there are countless universes in space,\n\tBut to me, she is the only universe.\n\tUntil I realized, she is the verse,\n\tWoven into my tangled heart.")
+print("\tTo others, they call her ‘she,’\n\tBut the word ‘she’ is the universe itself.\n\tYes, there are countless universes in space,\n\tBut to me, she is the only universe.\n\tUntil I realized, she is the verse,\n\tWoven into my tangled \"heart.\"")
+
