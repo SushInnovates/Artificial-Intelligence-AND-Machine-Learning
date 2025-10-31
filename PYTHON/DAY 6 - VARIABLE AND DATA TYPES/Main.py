11d96bf18e5606711a449d6de3933f140eda3c17
Sushant Sutar <sushant.sutR@gmail.com>
2025-10-31 20:46:27 +0530

    Add files via upload

---
+++ PYTHON/DAY 6 - VARIABLE AND DATA TYPES/Main.py
@@ -0,0 +1,32 @@
+A = 11 #integer
+B = True # boolean
+C = None # None
+D = "sushant" #string
+A1 = 2
+
+print(A)
+print(A+A1)
+print(B)
+print(C)
+print(D)
+
+print("Type of a is ",type(A))
+print("Type of a is ",type(B))
+print("Type of a is ",type(C))
+print("Type of a is ",type(D))
+
+E =  complex(11,2)
+print(E)
+print("Type of e is ",type(E))
+
+# list is a collection of different datatype
+list1 = [11,2,2.3,[-4,5],["Apple","Samsung"]]
+print(list1)
+
+# tuple is exactly as list but only thing is it is immutable(means it cant be changed)
+tuple = (("parrot","Sparrow"),("Lion","tiger"))
+print(tuple)
+
+# Dictionary is a collection of key- value pairs,it is a map data 
+dict = {"Name":"Perfectionist","Age":21,"isSheGenius":True}
+print(dict)
