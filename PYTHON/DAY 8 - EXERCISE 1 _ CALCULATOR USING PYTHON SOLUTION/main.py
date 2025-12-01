e0fd2c0d4c6a49f437140803a1e30ebbd3ddca48
Sushant Sutar <sushant.sutR@gmail.com>
2025-12-01 16:41:01 +0530

    Implement basic calculator operations in main.py

---
+++ PYTHON/DAY 8 - EXERCISE 1 _ CALCULATOR USING PYTHON SOLUTION/main.py
@@ -0,0 +1,17 @@
+
+a = int(input("Enter First Number : "))
+b = int(input("Enter First Number : "))
+add = a + b
+print("Addition of ",a," And ",b," is ",add)
+sub = a - b
+print("Substraction of ",a," And ",b," is ",sub)
+mul = a * b
+print("Multiplication of ",a," And ",b," is ",mul)
+expo = a ** a
+print("Exponential of ",a," And ",b," is ",expo)
+div = a / b
+print("Division of ",a," And ",b," is ",div)
+FD = a // b
+print("Floor Division of ",a," And ",b," is ",FD)
+mod = a % b
+print("Modulus of ",a," And ",b," is ",mod)
