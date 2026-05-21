55d73ac791218c5d7909927a8102dbfac010f84d
Sushant Sutar <sushant.sutR@gmail.com>
2026-05-21 14:11:50 +0630

    Add files via upload

---
+++ PYTHON/DAY 15 - WHILE LOOP/WhileLoop.py
@@ -0,0 +1,13 @@
+# in other languages there are while , do while , for 
+# while(condition){
+#     //code
+# }
+# in python there are only two loops 1> while loop 2> for loop
+# while condition :
+#     //code
+
+num = int(input("Enter Number : "))
+i = 1
+while i <= 10 :
+    print(num,"*",i,"=",num * i)
+    i+=1
