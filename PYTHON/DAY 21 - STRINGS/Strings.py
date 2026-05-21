084079a090457e3dec03b633c185c7243cacef07
Sushant Sutar <sushant.sutR@gmail.com>
2026-05-22 01:22:27 +0630

    Add files via upload

---
+++ PYTHON/DAY 21 - STRINGS/Strings.py
@@ -0,0 +1,23 @@
+# string :  string is the seqeunce of the unicode characters
+# 1> Creating Strings
+# 2> Accessing String
+# 3> Adding chars to strings
+# 4> Editing Strings
+# 5> Deleting Strings
+# 6> Operations on Strings
+# 7> String Functions
+
+#creating
+d = "python"
+print(d)
+
+print("its's Raining outside")
+
+e = '''Hello''' # multiline string
+print(e)
+
+f = """Hello""" # multiline string
+print(f)
+
+c = str("hello")
+print(c)
