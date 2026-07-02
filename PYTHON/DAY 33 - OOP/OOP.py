d8f61835a4f3b668357dabdb9b7356a2e9694b04
Sushant Sutar <sushant.sutR@gmail.com>
2026-07-02 22:52:20 +0630

    Add files via upload

---
+++ PYTHON/DAY 33 - OOP/OOP.py
@@ -0,0 +1,52 @@
+L = [1,2,3,4]
+# print(L.upper())
+
+# city='mumbai'
+# print(city.append('a'))
+
+# in python everythin g is an object
+# what is object ?
+# #       An object is a real instance of a class.
+# Class = Blueprint (design)
+# Object = Actual thing made from that blueprint
+# Real-life example
+
+# Suppose a company designs a phone.
+
+# Blueprint (Class):
+
+# Brand
+# Color
+# RAM
+# Storage
+
+# Actual phones (Objects):
+
+# Phone 1 → Black, 8GB RAM, 128GB
+# Phone 2 → Blue, 12GB RAM, 256GB
+
+# The blueprint is the class.
+# Each phone is an object.
+
+# 1 >  class - class is a blueprint
+a = 2
+print(type(a))
+
+# in python datatype is a class and when we make it variable it becomes the object of that class 
+# ex 
+L = [1,2,34]
+# L is a object of class List 
+
+
+# class is a blueprint how it will behave
+
+# class basic structure
+class Car:
+    color='blue'#data
+    model='sports'#data
+    def calculateAvgSpeed(km,time):
+        #some code
+
+# class name should be in PascelCase(First Letter of EVery word Capital)
+# data and function name should be in snake case(name created by lowercase which is connected using '_')
+ 
